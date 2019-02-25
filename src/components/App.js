52cbf4619d0bf56e48d8792d8abcfb2a8998d111
Al Buchala <albcf@yahoo.com>
2019-02-25 14:43:37 -0500

    Implement API get functionality

--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,26 @@
 import React from 'react';
+import axios from 'axios';
 import SearchBar from './SearchBar';
 
-const App = () => {
-  return (
-    <div className="container">
-      <SearchBar />
-    </div>
-  )
-};
+
+class App extends React.Component {
+  state = { books: [] };
+
+  handleSearchSubmit = async (searchQuery) => {
+    const response = await axios.get('https://www.googleapis.com/books/v1/volumes', {
+      params: { q: searchQuery}
+    });
+
+    this.setState({ books: response.data.items });
+  }
+  render() {
+    return (
+      <div className="container">
+        <SearchBar onSubmit={this.handleSearchSubmit} />
+        Found: {this.state.books.length} books
+      </div>
+    )
+  }
+}
 
 export default App;
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,14 +1,27 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  state = {searchQuery: ''};
+
+  handleFormSubmit = (event) => {
+    event.preventDefault();
+    this.props.onSubmit(this.state.searchQuery);
+  }
   render(){
     return (
       <div>
-        <form>
+        <form onSubmit={this.handleFormSubmit}>
           <div className="form-group">
-            <label for="bookSearchQuery">Book Search</label>
-            <input type="text" className="form-control" id="bookSearchQuery" />
+            <label htmlFor="bookSearchQuery">Book Search</label>
+            <input type="text"
+                   className="form-control"
+                   id="bookSearchQuery"
+                   value={this.state.searchQuery}
+                   onChange={(e) => this.setState({searchQuery: e.target.value})} />
           </div>
+          <button type="submit"
+                  className="btn btn-primary"
+                  disabled={!this.state.searchQuery}>Submit</button>
         </form>
       </div>
     )
