427e5127a2a7685c989d7287d1f4cab25248429b
Al Buchala <albcf@yahoo.com>
2019-02-26 14:34:13 -0500

    Refactor - extract Axior setup to own file

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,18 @@
 import React from 'react';
-import axios from 'axios';
+import googleBooks from '../api/googleBooks';
 import SearchBar from './SearchBar';
-
+import BookList from './BookList';
 
 class App extends React.Component {
   state = { books: [] };
 
   handleSearchSubmit = async (searchQuery) => {
-    const response = await axios.get('https://www.googleapis.com/books/v1/volumes', {
+    const response = await googleBooks.get('/books/v1/volumes', {
       params: { q: searchQuery}
     });
-
+    console.log(response.data.items);
+    console.log(response.data.items[0].volumeInfo.imageLinks.thumbnail);
+    console.log(response.data.items[1].volumeInfo.imageLinks.thumbnail);
     this.setState({ books: response.data.items });
   }
   render() {
@@ -18,6 +20,7 @@ class App extends React.Component {
       <div className="container">
         <SearchBar onSubmit={this.handleSearchSubmit} />
         Found: {this.state.books.length} books
+        <BookList books={this.state.books} />
       </div>
     )
   }
