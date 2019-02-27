56842ef9135a7a2d1de557537c434fdf5b0fead9
Al Buchala <albcf@yahoo.com>
2019-02-27 14:45:47 -0500

    Add styling to cover image

--- src/components/BookCoverImage.js
+++ src/components/BookCoverImage.js
@@ -10,7 +10,7 @@ class BookCoverImage extends React.Component {
   }
   render() {
     return (
-      <img className="thumbnail-fixed"
+      <img className="thumbnail-fixed img-thumbnail"
            src={this.state.thumbnail}
            alt='book cover'
            ></img>
