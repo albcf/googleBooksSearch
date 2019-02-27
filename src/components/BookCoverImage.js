db9d7d6cfb6c5ca26f14a4c837712e2e02c4bc53
Al Buchala <albcf@yahoo.com>
2019-02-27 12:41:32 -0500

    Add placeholder image for items with missing images

--- src/components/BookCoverImage.js
+++ src/components/BookCoverImage.js
@@ -5,7 +5,7 @@ class BookCoverImage extends React.Component {
   constructor(props){
     super();
     this.state = {
-      thumbnail: (props.imageLinks) ? props.imageLinks.thumbnail : '#'
+      thumbnail: (props.imageLinks) ? props.imageLinks.thumbnail : 'https://upload.wikimedia.org/wikipedia/commons/1/16/No_image_available_450_x_600.svg'
     };
   }
   render() {
